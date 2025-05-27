1f6e434e00ab4366811c2695a2594c1665d6e279
Ömer Cansın Kaya <omerates200394@gmail.com>
2025-05-28 01:17:25 +0300

    basic API elements were created, endpoints work fine

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import( 
-
+import (
+    "github.com/gin-gonic/gin"
+    "league-simulation/api-controller"
 )
 
-
-
 func main() {
+    r := gin.Default()
 
+    r.GET("/league-table", controller.GetLeagueTableHandler)
+    r.POST("/simulate-week/:week", controller.SimulateWeekHandler)
+    r.POST("/create-fixture", controller.CreateFixtureHandler)
+
+    // Eğer başka endpointler ekleyeceksen burada devam edebilirsin.
+
+    r.Run(":8080") // Sunucu 8080 portundan başlar
 }
+
